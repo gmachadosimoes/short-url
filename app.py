4b342758bcdf5ef6d3cb4d33c71c38ecaa823ddd
Gabriel Simoes <gmachadosimoes@gmail.com>
2020-12-23 10:51:44 +0000

    API handling added, needs to be completed

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 from flask import Flask
+import bitly_api
 
 app = Flask(__name__)
+
+BITLY_ACCESS_TOKEN = 'd88405c8b374e9aa3d981ff64ff7f09450e700af'
+access = bitly_api.Connection(access_token = BITLY_ACCESS_TOKEN)
+
+# This needs to be inserted into the HTML as a form with a button to return the shortened link
+full_link = input()
+short_url = access.shorten(full_link)
+# Basic functionality, needs to be wrapped around as a flask function to satisfy a GET request from the user
 
 @app.route('/')
