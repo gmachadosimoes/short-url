6dfde6597a79ad2fee1de2cf5cf6fc6927cc10c7
Gabriel Simoes <gmachadosimoes@gmail.com>
2020-12-28 23:37:09 +0000

    A few more steps

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ access = bitly_api.Connection(access_token = BITLY_ACCESS_TOKEN)
 # This needs to be inserted into the HTML as a form with a button to return the shortened link
 full_link = input()
 short_url = access.shorten(full_link)
+print('Short URL = ', short_url['url'])
 # Basic functionality, needs to be wrapped around as a flask function to satisfy a GET request from the user
 
 @app.route('/')
+def index():
+
